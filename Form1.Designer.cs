b15e848ce1561c4df7335b61e2e8433ef015ff09
Wang Weiqiang <wangwq_net@hotmail.com>
2013-10-18 17:28:13 +0800

    2013-10-18

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace SSIMvsPSNR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OriginalImage = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.compressedImage = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.PSNRValue = new System.Windows.Forms.Label();
+            this.SSIMvalue = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MSEValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.compressedImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OriginalImage
+            // 
+            this.OriginalImage.BackColor = System.Drawing.Color.White;
+            this.OriginalImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.OriginalImage.Location = new System.Drawing.Point(12, 23);
+            this.OriginalImage.Name = "OriginalImage";
+            this.OriginalImage.Size = new System.Drawing.Size(400, 350);
+            this.OriginalImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.OriginalImage.TabIndex = 0;
+            this.OriginalImage.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Original Image";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 443);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Compute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // compressedImage
+            // 
+            this.compressedImage.BackColor = System.Drawing.Color.White;
+            this.compressedImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.compressedImage.Location = new System.Drawing.Point(420, 23);
+            this.compressedImage.Name = "compressedImage";
+            this.compressedImage.Size = new System.Drawing.Size(400, 350);
+            this.compressedImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.compressedImage.TabIndex = 4;
+            this.compressedImage.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(140, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "PSNR:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(355, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "SSIM:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Processed Image";
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "Image files|*.jpg;*.bmp;*.png";
+            this.openFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog_FileOk);
+            // 
+            // PSNRValue
+            // 
+            this.PSNRValue.AutoSize = true;
+            this.PSNRValue.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.PSNRValue.ForeColor = System.Drawing.Color.Red;
+            this.PSNRValue.Location = new System.Drawing.Point(186, 469);
+            this.PSNRValue.Name = "PSNRValue";
+            this.PSNRValue.Size = new System.Drawing.Size(0, 13);
+            this.PSNRValue.TabIndex = 14;
+            // 
+            // SSIMvalue
+            // 
+            this.SSIMvalue.AutoSize = true;
+            this.SSIMvalue.ForeColor = System.Drawing.Color.Red;
+            this.SSIMvalue.Location = new System.Drawing.Point(397, 448);
+            this.SSIMvalue.Name = "SSIMvalue";
+            this.SSIMvalue.Size = new System.Drawing.Size(0, 13);
+            this.SSIMvalue.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Load Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(418, 380);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Load Image";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(147, 453);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "MSE:";
+            // 
+            // MSEValue
+            // 
+            this.MSEValue.AutoSize = true;
+            this.MSEValue.Location = new System.Drawing.Point(187, 453);
+            this.MSEValue.Name = "MSEValue";
+            this.MSEValue.Size = new System.Drawing.Size(0, 13);
+            this.MSEValue.TabIndex = 19;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 503);
+            this.Controls.Add(this.MSEValue);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.SSIMvalue);
+            this.Controls.Add(this.PSNRValue);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.compressedImage);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OriginalImage);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(840, 530);
+            this.MinimumSize = new System.Drawing.Size(840, 530);
+            this.Name = "Form1";
+            this.Text = "SSIM vs PSNR";
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.compressedImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox OriginalImage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox compressedImage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label PSNRValue;
+        private System.Windows.Forms.Label SSIMvalue;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label MSEValue;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SSIMvsPSNR
+{
+    public partial class Form1 : Form
+    {
+        const double C1=1,C2=1,C3=1;
+        
+        int flag = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            var fileImage = openFileDialog.FileName;
+            if (flag == 1)
+            {
+                OriginalImage.Load(fileImage);
+            }
+            if (flag == 2)
+            {
+                compressedImage.Load(fileImage);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //before processing image into 8x8 blocks, need to make the width and height of images is integral multiple of 8
+            //then cut the images into 8x8 blocks and save them into array.
+            Bitmap bm =(Bitmap)OriginalImage.Image;
+            Bitmap originalBitMap   = new Bitmap(OriginalImage.Image);
+            Bitmap compressedBitMap = new Bitmap(compressedImage.Image);
+            int width = OriginalImage.Image.Width;
+            int height = OriginalImage.Image.Height;
+
+            int modW = width % 8;
+            int modH = height % 8;
+            int newWidth=width;
+            if (modW > 0)
+            {
+                newWidth = width + (8 - modW);
+            }
+            
+            int newHeight=height;
+            if (modH > 0)
+            {
+                newHeight = height + (8 - modH);
+            }
+
+
+            Bitmap newOrBitmap = new Bitmap(newWidth,newHeight);
+            Bitmap newPrBitmap = new Bitmap(newWidth, newHeight);
+            Graphics graphicOrImage = Graphics.FromImage(newOrBitmap);
+            Graphics graphicPrImage = Graphics.FromImage(newPrBitmap);
+            graphicOrImage.DrawImage(originalBitMap, 0, 0, width, height);
+            graphicPrImage.DrawImage(compressedBitMap, 0, 0, width, height);
+
+            List<Bitmap> orBlockArray = new List<Bitmap>();
+            List<Bitmap> prBlockArray = new List<Bitmap>();
+
+            for (int i = 0; i < newWidth / 8; i++)
+            {
+                for (int j = 0; j < newHeight / 8; j++)
+                {
+                    Bitmap orBlock = new Bitmap(8, 8);
+                    orBlock = Cut(newOrBitmap, i * 8, j * 8, 8, 8);
+                    orBlockArray.Add(orBlock);
+
+                    Bitmap prBlock = new Bitmap(8, 8);
+                    prBlock = Cut(newPrBitmap, i * 8, j * 8, 8, 8);
+                    prBlockArray.Add(prBlock);
+                }
+            }
+            //end preparing images
+
+
+            double PSNR = GetPSNR(orBlockArray, prBlockArray);
+            PSNRValue.Text = PSNR.ToString();
+
+            double SSIM = GetSSIM(orBlockArray, prBlockArray);
+
+            SSIMvalue.Text = SSIM.ToString();
+            
+        }
+
+        private double GetPSNR(List<Bitmap> x, List<Bitmap> y)
+        {
+            double MSE = 0;
+            for (int i = 0; i < x.Count(); i++)
+            {
+                MSE += ComputBlockMSE(x[i], y[i]);
+            }
+
+            MSE = MSE / x.Count();
+            if (MSE == 0)
+            {
+                MSE = 1;
+            }
+            MSEValue.Text = MSE.ToString();
+            double PSNR = 10 * Math.Log10((255 * 255) / MSE);
+            return PSNR;
+        }
+
+        private double GetSSIM(List<Bitmap> x, List<Bitmap> y)
+        {
+            double SSIM = 0;
+            for (int i = 0; i < x.Count(); i++)
+            {
+                double l = GetSSIMLuminance(x[i], y[i]);
+                double c = GetSSIMContrast(x[i], y[i]);
+                double s=GetSSIMStructural(x[i], y[i]);
+                SSIM += (l * c * s);
+            }
+            SSIM = SSIM / x.Count();
+
+            return SSIM;
+        }
+
+        private Bitmap Cut(Bitmap b, int StartX, int StartY, int iWidth, int iHeight)
+        {
+            if (b == null) { return null; }
+            int w = b.Width;
+            int h = b.Height;
+            if (StartX >= w || StartY >= h) { return null; } if (StartX + iWidth > w) { iWidth = w - StartX; }
+            if (StartY + iHeight > h) { iHeight = h - StartY; } try
+            {
+                Bitmap bmpOut = new Bitmap(iWidth, iHeight, PixelFormat.Format24bppRgb);
+                Graphics g = Graphics.FromImage(bmpOut); g.DrawImage(b, new Rectangle(0, 0, iWidth, iHeight), new Rectangle(StartX, StartY, iWidth, iHeight), GraphicsUnit.Pixel);
+                g.Dispose(); 
+                return bmpOut;
+            }
+            catch { return null; }
+        }
+
+        private double GetLuminanceByColor(Color color)
+        {
+            return 0.2126 * color.R + 0.7152 * color.G + 0.0722 * color.B;
+        }
+
+        private double ComputBlockMSE(Bitmap x,Bitmap y)
+        {
+            double MSE = 0;
+            double xLuminance = GetAvgLuminance(x);
+            double yLuminance = GetAvgLuminance(y);
+
+            MSE = (xLuminance - yLuminance)*(xLuminance - yLuminance);
+            return MSE;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            flag = 1;
+            DialogResult fileDialog = openFileDialog.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            flag = 2;
+            DialogResult fileDialog = openFileDialog.ShowDialog();
+        }
+
+        private double GetSSIMLuminance(Bitmap x, Bitmap y)
+        {
+            double v = 0;
+
+            double xLuminance = GetAvgLuminance(x);
+            double yLuminance = GetAvgLuminance(y);
+
+            v = (2 * xLuminance * yLuminance+C1) / (xLuminance * xLuminance + yLuminance * yLuminance+C1);
+
+            return v;
+        }
+
+        private double GetSSIMContrast(Bitmap x, Bitmap y)
+        {
+            double v = 0;
+            double xVariance=GetVariance(x);
+            double yVariance=GetVariance(y);
+            v=(2 * Math.Sqrt(xVariance * yVariance) + C2) / (xVariance + yVariance + C2);
+            return v;
+        }
+
+        private double GetSSIMStructural(Bitmap x, Bitmap y)
+        {
+            double v = 0;
+            double co = GetCovariance(x, y);
+            double vx = GetVariance(x);
+            double vy = GetVariance(y);
+            v = co + C3 / (vx * vy + C3);
+            return v;
+        }
+
+        //Get average luminance value of image
+        private double GetAvgLuminance(Bitmap bitmap){
+            double avgLuminance = 0;
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    Color color = bitmap.GetPixel(i, j);
+                    avgLuminance +=(0.2126 * color.R + 0.7152 * color.G + 0.0722 * color.B);
+                }
+            }
+
+            return avgLuminance / (bitmap.Width*bitmap.Height);
+        }
+
+        //Get variance value of image, before sqrt
+        private double GetVariance(Bitmap bitmap)
+        {
+            //每个像素点的灰度值减去图像平均灰度值的平方和除以总的像素个数
+            double v = 0;
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    try
+                    {
+                        Color or_color = bitmap.GetPixel(i, j);
+                        double or_luminance = 0.2126 * or_color.R + 0.7152 * or_color.G + 0.0722 * or_color.B;
+
+                        v += or_luminance;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+            double avgV = v / 64;
+            v = 0;
+
+            for (int i = 0; i < bitmap.Width; i++)
+            {
+                for (int j = 0; j < bitmap.Height; j++)
+                {
+                    try
+                    {
+                        Color or_color = bitmap.GetPixel(i, j);
+                        double or_luminance = 0.2126 * or_color.R + 0.7152 * or_color.G + 0.0722 * or_color.B;
+                        v += (or_luminance - avgV) * (or_luminance - avgV);
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+
+            return v / (bitmap.Width*bitmap.Height);
+        }
+
+        
+        //Get covariance value of image1 and image2
+        private double GetCovariance(Bitmap x, Bitmap y)
+        {
+            //每个像素点的灰度值减去图像平均灰度值的平方和除以总的像素个数
+            double v = 0;
+
+            for (int i = 0; i < x.Width; i++)
+            {
+                for (int j = 0; j < x.Height; j++)
+                {
+                    try
+                    {
+                        Color or_color = x.GetPixel(i, j);
+                        double or_luminance = 0.2126 * or_color.R + 0.7152 * or_color.G + 0.0722 * or_color.B;
+
+                        v += or_luminance;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+            double x_avgV = v / (x.Width*x.Height);
+
+            v = 0;
+            for (int i = 0; i < y.Width; i++)
+            {
+                for (int j = 0; j < y.Height; j++)
+                {
+                    try
+                    {
+                        Color or_color = y.GetPixel(i, j);
+                        double or_luminance = 0.2126 * or_color.R + 0.7152 * or_color.G + 0.0722 * or_color.B;
+
+                        v += or_luminance;
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+            double y_avgV = v / (x.Width * x.Height);
+
+
+
+            v = 0;
+            for (int i = 0; i < x.Width; i++)
+            {
+                for (int j = 0; j < x.Height; j++)
+                {
+                    try
+                    {
+                        Color x_color = x.GetPixel(i, j);
+                        double x_luminance = 0.2126 * x_color.R + 0.7152 * x_color.G + 0.0722 * x_color.B;
+
+                        Color y_color = y.GetPixel(i, j);
+                        double y_luminance = 0.2126 * y_color.R + 0.7152 * y_color.G + 0.0722 * y_color.B;
+
+                        v += (x_luminance - x_avgV) * (y_luminance - y_avgV);
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+
+            return v/(x.Height*x.Width);
+        }
+    }
+}
